1b8c5cf96ddfd0a9af1c2aaffb1b6430fa70b8aa
Klemens David Morgenstern <klemens.morgenstern@gmx.net>
2019-02-05 09:16:18 +0800

    added NUnit

--- MsTestExample/Class1.cs
+++ MsTestExample/Class1.cs
@@ -1,6 +1,36 @@
-﻿namespace CSharpExample
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpExample
 {
+    [TestClass]
     public class Class1
     {
+        [TestMethod]
+        public void func1()
+        {
+            Assert.Fail();
+        }
+
+        void nested()
+        {
+            Assert.AreEqual(3,2);
+
+        }
+        
+        [TestMethod]
+        public void func2()
+        {
+            nested();
+        }
+
+        [DataTestMethod]
+        [DataRow(-1)]
+        [DataRow(0)]
+        [DataRow(1)]
+        public void ReturnFalseGivenValuesLessThan2(int value)
+        {
+            Assert.IsFalse(value >= 0, $"{value} should not be prime");
+        }
+        
     }
 }
---
+++ NUnitExample/Tests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NUnitExample
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.True(true);
+            Assert.True(false);
+        }
+
+
+        void nested()
+        {
+            Assert.Fail();
+        }
+        [Test]
+        public void Test3()
+        {
+            this.nested();
+        }
+
+    }
+}
